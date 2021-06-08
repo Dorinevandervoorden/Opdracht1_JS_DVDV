d758d678b3fd119523185aeb8bbac62d1249771b
Dorine van der Voorden <84196122+Dorinevandervoorden@users.noreply.github.com>
2021-06-08 18:37:02 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,34 @@
+console.log("Hello Winc Academy")
+
+let name = '4' + '4'
+console.log(name);
+
+let age = 4 + 4;
+console.log(age);
+
+let birthDate = 4 * 4 - 4 + 4;
+console.log(birthDate);
+
+function myFunction() {
+    let p = 45;
+    let progress = p % 100;
+    document.getElementById("demo").innerHTML = progress;
+}
+
+function myTest() {
+    let y = 2;
+    let uitkomst = y + 1;
+    document.getElementById("antwoord").innerHTML = uitkomst;
+
+    let huh = 'huh???? maar dat klopt niet!?!?!?';
+    if (uitkomst = 3) {
+        document.getElementById("huh").innerHTML = huh;
+    }
+
+    let haha = 'haha, grapjas!';
+    if (uitkomst = 3) {
+        document.getElementById("haha").innerHTML = haha;
+    }
+
+}
+
